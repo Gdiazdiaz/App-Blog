709378c8261e47d0a3f5b3fb7246f46026dd54ed
Glenda Diaz <glendadiazah@outlook.com>
2023-03-30 14:21:02 -0600

    fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,7 +5,7 @@ class Like < ApplicationRecord
   after_save :update_likes_counter
 
   private
-  
+
   def update_likes_counter
     post.increment!(:likesCounter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   private
+
   def update_posts_counter
     author.increment!(:postsCounter)
   end
