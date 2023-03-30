aba02231d5b86998f93dc1863100440c725c4125
Glenda Diaz <glendadiazah@outlook.com>
2023-03-30 12:38:23 -0600

    add methods to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def self.update_posts_counter
+    author.increment!(:postsCounter)
+  end
+
+  def self.recent_five_comments(title)
+    post = Post.find_by(title:).id
+    Comment.where(post_id: post).order(created_at: :desc).limit(3)
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: 'author_id'
     has_many :comments, foreign_key: 'author_id'
     has_many :likes, foreign_key: 'author_id'
+
+    def self.recent_three_posts(name)
+        author = User.find_by(name:).id
+        Post.where(author_id: author).order(created_at: :desc).limit(3)
+    end
 end
