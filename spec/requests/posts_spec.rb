fa542db4f7e9b7f0778fc468961aaa126c69d86f
Glenda Diaz <glendadiazah@outlook.com>
2023-04-04 15:58:40 -0600

    Add factory files, faker generators and the Request spec files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,39 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before { get user_posts_path(user) }
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+  
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('List of posts')
     end
   end
 
   describe 'GET /show' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:post) { FactoryBot.create(:post, author: user) }
+
+    before { get user_post_path(user, post) }
+
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('Specific post')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,39 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    
+    before { get users_path }
+
     it 'returns http success' do
       get '/users/index'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('All users')
+    end
   end
 
-  describe 'GET /show' do
+  describe 'GET #show' do
+
+    let(:user) { FactoryBot.create(:user) }
+
+    before { get user_path(user) }
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      expect(response.body).to include('specific user')
     end
   end
 end
