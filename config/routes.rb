392bfb372326fbd79c9853cb9d9ff63f71843e61
Glenda Diaz <glendadiazah@outlook.com>
2023-04-12 10:27:55 -0600

    Add configurations for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
