73b6345ad8793cae2ebfe8d21317692cfd724aa3
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-15 01:52:21 +0600

    Add function to create a comment in a post.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,11 @@
-      class Api::UsersController < Api::ApiController
+class Api::UsersController < Api::ApiController
+  def index
+    @users = User.all.order(created_at: :desc)
+    render json: @users, status: :ok
+  end
 
-        def index
-          @users = User.all.order(created_at: :desc)
-          render json: @users, status: :ok
-        end
-
-        def show
-            @user = User.find(params[:id])
-            render json: @user, status: :ok
-        end
-      end
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
+end
